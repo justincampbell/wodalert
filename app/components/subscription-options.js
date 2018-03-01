4472a66cc1472a64f8e6ceab15d235c567d7a6b1
Justin Campbell <justin@justincampbell.me>
2018-03-01 11:29:45 -0500

    Show more options link

--- app/components/subscription-options.js
+++ app/components/subscription-options.js
@@ -4,6 +4,8 @@ import { computed } from "@ember/object";
 export default Component.extend({
   subscription: null,
 
+  showAdvancedOptions: false,
+
   messageLimit: computed("subscription.characterLimit", function() {
     return this.get("subscription.characterLimit") / 160;
   }),
@@ -11,6 +13,10 @@ export default Component.extend({
   actions: {
     incrementCharacterLimit(by) {
       this.get("subscription").incrementCharacterLimit(by);
+    },
+
+    toggleAdvancedOptions() {
+      this.toggleProperty("showAdvancedOptions");
     }
   }
 });
