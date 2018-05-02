da447b3289009ed91825ff77ad0cdfa4d84f2e0d
Justin Campbell <justin@justincampbell.me>
2018-05-02 11:54:50 -0400

    Fix app host

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,10 @@
 import DS from "ember-data";
 import { computed } from "@ember/object";
 import { inject as service } from "@ember/service";
+import ENV from "wodalert/config/environment";
 
 export default DS.JSONAPIAdapter.extend({
+  host: ENV.APP.host,
   authentication: service(),
 
   headers: computed("authentication.isAuthenticated", function() {
--- app/services/ajax.js
+++ app/services/ajax.js
@@ -1,13 +1,15 @@
 import AjaxService from "ember-ajax/services/ajax";
 import { computed } from "@ember/object";
+import ENV from "wodalert/config/environment";
 
 export default AjaxService.extend({
+  host: ENV.APP.host,
   token: null,
 
   /**
-  * headers are the default headers. These are overridden by headers passed
-  * from a request method.
-  */
+   * headers are the default headers. These are overridden by headers passed
+   * from a request method.
+   */
   headers: computed("token", {
     get() {
       let headers = {};
