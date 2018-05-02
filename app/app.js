3faec07067d777d06f7e27b6059e2a2a6cf1f0b9
Justin Campbell <justin@justincampbell.me>
2018-05-02 11:41:04 -0400

    Default active class to is-active

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
-import Application from '@ember/application';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import loadInitializers from "ember-load-initializers";
+import Application from "@ember/application";
+import Ember from "ember";
+import Resolver from "./resolver";
+import config from "./config/environment";
+
+Ember.LinkComponent.reopen({
+  activeClass: "is-active",
+});
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
 });
 
 loadInitializers(App, config.modulePrefix);
